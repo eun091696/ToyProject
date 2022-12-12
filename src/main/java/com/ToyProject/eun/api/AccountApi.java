0b129fc5567ddaae241f223607ff78e35cf34f2d
Changeun An <eun091696@gmail.com>
2022-12-13 00:28:03 +0900

    12.13 쿼리문 오류 해결 (Api @RequsetBody 누락)

--- src/main/java/com/ToyProject/eun/api/AccountApi.java
+++ src/main/java/com/ToyProject/eun/api/AccountApi.java
@@ -6,6 +6,7 @@ import com.ToyProject.eun.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class AccountApi {
     private final AccountService accountService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(AccountDto accountDto) throws Exception {
+    public ResponseEntity<?> register(@RequestBody AccountDto accountDto) throws Exception {
         accountService.register(accountDto);
 
         return ResponseEntity.created(URI.create("/login")).body(new CMRespDto<>("Success", accountDto.getUsername()));
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -7,15 +7,19 @@ class Api {
         return this.#instance;
     }
 
-    getRegisterApi(user) {
+    constructor() {
+        this.getRegisterApi();
+    }
+
+    getRegisterApi() {
         const registerBtn = document.querySelector(".register");
 
-        // registerBtn.onclick = () => {
-        //     let user = {
-        //         "username" : document.querySelectorAll(".login-content input")[0].value,
-        //         "password" : document.querySelectorAll(".login-content input")[1].value,
-        //         "passwordChk" : document.querySelectorAll(".login-content input")[2].value
-        //     }
+        registerBtn.onclick = () => {
+            let user = {
+                "username" : document.querySelector(".username").value,
+                "password" : document.querySelectorAll(".login-content input")[1].value,
+                "passwordChk" : document.querySelectorAll(".login-content input")[2].value
+            }
             $.ajax({
                 async: false,
                 type: "post",
@@ -25,46 +29,47 @@ class Api {
                 dataType: "json",
                 success: (response) => {
                     console.log(response.data);
+                    console.log("실행되요");
                 },
                 error: (error) => {
                     console.log(error);
                 }
             });
         }
-    // }
-}
-
-class Event {
-    static #instance = null;
-    static getInstance() {
-        if(this.#instance == null) {
-            this.#instance = new Event();
-        }
-        return this.#instance;
-    }
-
-
-    constructor() {
-        this.registerEvent();
-    }
-
-    registerEvent() {
-        const registerBtn = document.querySelector(".register");
-
-        registerBtn.onclick = () => {
-            let user = {
-                "username" : document.querySelector(".username").value,
-                "password" : document.querySelector(".password").value,
-                "passwordChk" : document.querySelector(".pw-chk").value
-            }
-            console.log(user);
-            Api.getInstance().getRegisterApi(user);
-        }
-
     }
 }
+
+// class Event {
+//     static #instance = null;
+//     static getInstance() {
+//         if(this.#instance == null) {
+//             this.#instance = new Event();
+//         }
+//         return this.#instance;
+//     }
+
+
+//     constructor() {
+//         this.registerEvent();
+//     }
+
+//     registerEvent() {
+//         const registerBtn = document.querySelector(".register");
+
+//         registerBtn.onclick = () => {
+//             let user = {
+//                 "username" : document.querySelector(".username").value,
+//                 "password" : document.querySelector(".password").value,
+//                 "passwordChk" : document.querySelector(".pw-chk").value
+//             }
+//             new Api();
+//             console.log(new Api());
+//         }
+
+//     }
+// }
 
 window.onload = () => {
-    // Api.getInstance().getRegisterApi();
-    new Event();
+    Api.getInstance().getRegisterApi();
+    // new Event();
 }
